4b5d707b19edd57b9738f3d4c93056fd24c7b449
q756796724 <84502311+q756796724@users.noreply.github.com>
2022-02-12 08:10:16 +0800

    Update weiwei.js
    v1.2.1

--- weiwei.js
+++ weiwei.js
@@ -97,7 +97,7 @@ var topActivity = "";
 var MAIN_PKG = "com.fanqie.cloud";
 var PKG_NAME = "com.tencent.mm";
 var MAIN_PAGE = "com.tencent.mm.ui.LauncherUI";
-var versionNum = "v1.2.0";
+var versionNum = "v1.2.1";
 
 
 function jm() {
@@ -329,7 +329,7 @@ ui.start1.on("click", () => {
                         home()
                         sleep(1500)
                         if(device.brand=='Meizu'){
-                            sml_move(device.width*0.3, device.height-10, device.width*0.5, device.height*0.6, 1000);
+                            sml_move(device.width*0.3, device.height-10, device.width*0.5, device.height*0.6, 3000);
                         }else if(device.brand=='Xiaomi'){
                             recents()
                         }
