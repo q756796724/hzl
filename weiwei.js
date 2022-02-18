7c1d5b17d5d45ca87bfc0fa325b36db260990558
q756796724 <84502311+q756796724@users.noreply.github.com>
2022-02-19 03:49:37 +0800

    Update weiwei.js
    v1.3.8 修改内存阈值

--- weiwei.js
+++ weiwei.js
@@ -97,7 +97,7 @@ var topActivity = "";
 var MAIN_PKG = "com.fanqie.cloud";
 var PKG_NAME = "com.tencent.mm";
 var MAIN_PAGE = "com.tencent.mm.ui.LauncherUI";
-var versionNum = "v1.3.6";
+var versionNum = "v1.3.8";
 
 
 function jm() {
@@ -325,7 +325,7 @@ ui.start1.on("click", () => {
 
                 function 清理后台() {
                     if (device.brand == 'Xiaomi') {
-                        if (device.getAvailMem() / 1024 / 1024 < 350) {
+                        if (device.getAvailMem() / 1024 / 1024 < 250) {
                             log(new Date().toLocaleString() + "-" + "可用内存" + device.getAvailMem() / 1024 / 1024);
                             log(new Date().toLocaleString() + "-" + "清理后台");
                             home()
@@ -342,7 +342,7 @@ ui.start1.on("click", () => {
                             log(new Date().toLocaleString() + "-" + "可用内存" + device.getAvailMem() / 1024 / 1024);
                         }
                     } else if (device.brand == 'Meizu') {
-                        if (device.getAvailMem() / 1024 / 1024 < 350) {
+                        if (device.getAvailMem() / 1024 / 1024 < 250) {
                             log(new Date().toLocaleString() + "-" + "可用内存" + device.getAvailMem() / 1024 / 1024);
                             log(new Date().toLocaleString() + "-" + "清理后台");
                             home()
@@ -365,7 +365,7 @@ ui.start1.on("click", () => {
                             log(new Date().toLocaleString() + "-" + "可用内存" + device.getAvailMem() / 1024 / 1024);
                         }
                     } else {
-                        if (device.getAvailMem() / 1024 / 1024 < 350) {
+                        if (device.getAvailMem() / 1024 / 1024 < 300) {
                             log(new Date().toLocaleString() + "-" + "可用内存" + device.getAvailMem() / 1024 / 1024);
                             log(new Date().toLocaleString() + "-" + "清理后台");
                             home()
