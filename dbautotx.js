d98a005ae8f645252107687aa998a4882989d4e1
q756796724 <84502311+q756796724@users.noreply.github.com>
2022-05-06 22:50:59 +0800

    Update dbautotx.js
    v1.0.3修复每日初始化问题

--- dbautotx.js
+++ dbautotx.js
@@ -1,10 +1,14 @@
 //duobaoAutoTx
-var versionNum = "v1.0.2";
+var versionNum = "v1.0.3";
 auto.waitFor()//检查无障碍服务是否已经启用，会在在无障碍服务启动后继续运行。
 
 toastLog(device.brand);
 toastLog("版本号:" + versionNum);
 storage = storages.create("fanqiecloud配置");
+if(storage.get("date")==undefined){
+    toastLog("初始化");
+    storage.put("date", new Date().toLocaleDateString());
+}
 var dbTodayTxCount = storage.get("dbTodayTxCount", 0);
 var thread = threads.start(function () {
     setInterval(() => { }, 1000);
@@ -20,10 +24,11 @@ app.launch("com.frp.fun");
 sleep(20000)
 for (; ;) {
     var nowHour = new Date().getHours();
-    if (storage.get("date", new Date().toLocaleDateString()) != new Date().toLocaleDateString()) {
-        toastLog("初始化配置");
+    if (storage.get("date") != new Date().toLocaleDateString()) {
+        toastLog("每日初始化配置");
         storage.put("date", new Date().toLocaleDateString());
         storage.put("dbTodayTxCount", 0);
+        dbTodayTxCount = storage.get("dbTodayTxCount");
     }
     if (currentPackage() == "com.frp.fun") {
         if (packageName("com.frp.fun").textContains("请检查您的网络").findOnce() != null) {
@@ -49,6 +54,7 @@ for (; ;) {
             if ((nowHour > 15 && nowHour < 20 && dbTodayTxCount < 1) || (nowHour > 17 && nowHour < 20 && dbTodayTxCount < 2) || (nowHour > 19 && nowHour < 20 && dbTodayTxCount < 3)) {
                 cBtn = packageName("com.frp.fun").id('tixianlayout').findOnce()
                 if (cBtn != null) {
+                    toastLog("dbTodayTxCount="+dbTodayTxCount);
                     dbTodayTxCount++
                     storage.put("dbTodayTxCount", dbTodayTxCount);
                     cBtn.click()
