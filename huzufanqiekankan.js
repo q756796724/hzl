c93953679441ad2e577ed141ae72bad9154ae9c8
q756796724 <84502311+q756796724@users.noreply.github.com>
2022-05-12 01:23:52 +0800

    Update huzufanqiekankan.js
    v3.0.8 修复ui操作bug

--- huzufanqiekankan.js
+++ huzufanqiekankan.js
@@ -52,7 +52,10 @@ ui.emitter.on("resume", function () {
 });
 var thread1 = threads.start(function () {
     setTimeoutA = setTimeout(() => {
-        ui.ok.click()
+        ui.run(()=>{
+            //这里写针对UI的操作
+            ui.ok.click()
+        });
         sleep(8000)
         let kbtn=textMatches(/(立即开始|.*立即开始.*)/).findOne(3000);
         if (kbtn != null) {
@@ -91,7 +94,7 @@ ui.ok.click(function () {
         var MAIN_PKG = "com.fanqie.cloud";
         var PKG_NAME = "com.tencent.mm";
         var MAIN_PAGE = "com.tencent.mm.ui.LauncherUI";
-        var versionNum = "v3.0.7";
+        var versionNum = "v3.0.8";
         var readNum = 0;//最近获取到的阅读次数
         var retryCount = 0;//进入页面重试次数
         var todayTxCount = 0;
