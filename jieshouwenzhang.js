512423dc6578bc23690c020cd5d6cbf9d2dccd65
q756796724 <84502311+q756796724@users.noreply.github.com>
2022-07-04 16:58:12 +0800

    Update jieshouwenzhang.js
    接收v1.1.5 逻辑优化

--- jieshouwenzhang.js
+++ jieshouwenzhang.js
@@ -186,7 +186,7 @@ ui.ok.click(function () {
         var MAIN_PKG = "com.fanqie.cloud";
         var PKG_NAME = "com.tencent.mm";
         var MAIN_PAGE = "com.tencent.mm.ui.LauncherUI";
-        var versionNum = "接收v1.1.4";
+        var versionNum = "接收v1.1.5";
         var readNum = 0;//最近获取到的阅读次数
         var retryCount = 0;//进入页面重试次数
         var todayTxCount = 0;
@@ -688,7 +688,8 @@ ui.ok.click(function () {
                                     latestLink.click();
                                     reducejieshouCount()
                                     阅读到底();
-                                    sleep(random(1800000, 2000000))
+                                    sleep(random(2000000, 2400000))
+                                    i=i+200
                                     wBtn = packageName("com.tencent.mm").className("android.widget.TextView").textMatches(/(大家庭.*)/).findOnce();//id=ipv
                                     if (wBtn != null) {
                                         addjieshouCount()
@@ -708,7 +709,8 @@ ui.ok.click(function () {
                                         latestLink.click();
                                         reducejieshouCount()
                                         阅读到底();
-                                        sleep(random(1800000, 2000000))
+                                        sleep(random(2000000, 2400000))
+                                        i=i+200
                                         wBtn = packageName("com.tencent.mm").className("android.widget.TextView").textMatches(/(大家庭.*)/).findOnce();//id=ipv
                                         if (wBtn != null) {
                                             addjieshouCount()
@@ -745,13 +747,13 @@ ui.ok.click(function () {
                 } else {
                     console.error("not found 大家庭")
                     关闭应用(PKG_NAME);
-                    lunSleep();
+                    sleep(300000);
                 }
 
             } else {
                 console.error("not found bg1")
                 关闭应用(PKG_NAME);
-                lunSleep();
+                sleep(300000);
             }
         }
         function 进入收藏() {
@@ -974,6 +976,7 @@ ui.ok.click(function () {
         function onMainPage() {
             log("进入v成功");
             jieshouwenzhang();
+            返回v首页();
         }
 
         function quGuan(sleepTime) {
