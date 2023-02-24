f605a7cf61e00ef19f339d6b2b3841bff2d8c7c8
q756796724 <756796724@qq.com>
2023-02-24 20:30:03 +0800

    1

--- fenxiangfanqiekankan.js
+++ fenxiangfanqiekankan.js
@@ -185,7 +185,7 @@ ui.ok.click(function () {
         var MAIN_PKG = "com.fanqie.cloud";
         var PKG_NAME = "com.tencent.mm";
         var MAIN_PAGE = "com.tencent.mm.ui.LauncherUI";
-        var versionNum = "番茄分享v6.3.1";
+        var versionNum = "番茄分享v6.3.2";
         var readNum = 0;//最近获取到的阅读次数
         var retryCount = 0;//进入页面重试次数
         var todayTxCount = 0;
