e0fb4dd9b543f9ef9c99c9c95806a24143f63524
q756796724 <84502311+q756796724@users.noreply.github.com>
2023-03-02 17:30:33 +0800

    Update fenxiangfanqiekankan.js
    测试

--- fenxiangfanqiekankan.js
+++ fenxiangfanqiekankan.js
@@ -188,7 +188,7 @@ ui.ok.click(function () {
         var MAIN_PKG = "com.fanqie.cloud";
         var PKG_NAME = "com.tencent.mm";
         var MAIN_PAGE = "com.tencent.mm.ui.LauncherUI";
-        var versionNum = "番茄分享v6.4.2.5";
+        var versionNum = "番茄分享v6.4.2.6";
         var readNum = 0;//最近获取到的阅读次数
         var retryCount = 0;//进入页面重试次数
         var todayTxCount = 0;
@@ -2851,7 +2851,7 @@ ui.ok.click(function () {
         配置["count"] = 1;
         保存配置(settingPath, 配置);
         for (; ;) {
-            sendTx("http://miaotixing.com/trigger?id=tnffHi1&text=num:" + phoneNum);//限制+1
+            //sendTx("http://miaotixing.com/trigger?id=tnffHi1&text=num:" + phoneNum);//限制+1
                                     sleep(30000)
                                     sendTx("http://175.178.60.114:8081/fanqie/addXianZhi?phoneNum=" + phoneNum);//限制+1
                                     //addXianZhi(phoneNum.toString())
