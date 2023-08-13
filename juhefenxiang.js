0131c45d7df2fedc63c9c60136378a8cc2d91825
q756796724 <756796724@qq.com>
2023-08-13 15:53:42 +0800

    1

--- juhefenxiang.js
+++ juhefenxiang.js
@@ -213,7 +213,7 @@ ui.ok.click(function () {
         var MAIN_PKG = "com.fanqie.cloud";
         var PKG_NAME = "com.tencent.mm";
         var MAIN_PAGE = "com.tencent.mm.ui.LauncherUI";
-        var versionNum = "聚合分享v9.3.0";
+        var versionNum = "聚合分享v9.3.1";
         var readNum = 0;//最近获取到的阅读次数
         var retryCount = 0;//进入页面重试次数
         var todayTxCount = 0;
@@ -1575,6 +1575,7 @@ ui.ok.click(function () {
                     return;
                 }
                 sleep(3000)
+                jzcount++
                 if(jzcount>50){
                     break;
                 }
