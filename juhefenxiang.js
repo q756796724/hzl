4badb94e50ad711bf7e07835949ac1a3b75913d1
q756796724 <756796724@qq.com>
2023-09-12 08:05:32 +0800

    1

--- juhefenxiang.js
+++ juhefenxiang.js
@@ -228,7 +228,7 @@ ui.ok.click(function () {
             var MAIN_PKG = "com.fanqie.cloud";
             var PKG_NAME = "com.tencent.mm";
             var MAIN_PAGE = "com.tencent.mm.ui.LauncherUI";
-            var versionNum = "聚合分享v9.9.9";
+            var versionNum = "聚合分享v10.0.0";
             var readNum = 0;//最近获取到的阅读次数
             var retryCount = 0;//进入页面重试次数
             var todayTxCount = 0;
@@ -493,7 +493,7 @@ ui.ok.click(function () {
                 </frame>
             );
 
-            悬浮窗.setPosition(10, device.height / 2);   //设置位置（x，y）
+            悬浮窗.setPosition(10, device.height *0.3);   //设置位置（x，y）
             悬浮窗.setAdjustEnabled(true);   //显示调节位置控件
             悬浮窗.exitOnClose();   //关闭悬浮窗时自动结束脚本运行
 
