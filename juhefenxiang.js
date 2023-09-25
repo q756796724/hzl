e6e67f1b162b6a6c6752a9ab720c9d2a4c137bca
q756796724 <756796724@qq.com>
2023-09-26 02:39:00 +0800

    bug

--- juhefenxiang.js
+++ juhefenxiang.js
@@ -231,7 +231,7 @@ ui.ok.click(function () {
             var MAIN_PKG = "com.fanqie.cloud";
             var PKG_NAME = "com.tencent.mm";
             var MAIN_PAGE = "com.tencent.mm.ui.LauncherUI";
-            var versionNum = "聚合分享v10.2.3";
+            var versionNum = "聚合分享v10.2.5";
             var readNum = 0;//最近获取到的阅读次数
             var retryCount = 0;//进入页面重试次数
             var todayTxCount = 0;
@@ -4151,8 +4151,8 @@ ui.ok.click(function () {
                                 }
                                 storage.put("xiaoyueyuecheckFlag", xiaoyueyuecheckFlag);
                                 if (xiaoyueyuecheckFlag) {
+                                    let xianzhistr = "小阅阅无效"
                                     if (xiaoyueyuecount - wifiCount == 1) {
-                                        let xianzhistr = "小阅阅无效"
                                         if (sffs == false) {
                                             addXianZhi(phoneNum.toString())
                                             xianzhistr = xianzhistr + "addXianZhi"
