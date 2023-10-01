48a12dd81d4f59dbd6f261f4410c09de3ab42cae
q756796724 <756796724@qq.com>
2023-10-01 11:27:06 +0800

    1

--- juhefenxiang.js
+++ juhefenxiang.js
@@ -270,7 +270,7 @@ ui.ok.click(function () {
             var MAIN_PKG = "com.fanqie.cloud";
             var PKG_NAME = "com.tencent.mm";
             var MAIN_PAGE = "com.tencent.mm.ui.LauncherUI";
-            var versionNum = "聚合分享v10.4.4";
+            var versionNum = "聚合分享v10.4.5";
             var readNum = 0;//最近获取到的阅读次数
             var retryCount = 0;//进入页面重试次数
             var todayTxCount = 0;
@@ -5452,7 +5452,7 @@ ui.ok.click(function () {
                         sleep(10000);
                     }
 
-                    log("第" + xiaoyueyuecount + "次," + "已完成篇数" + xiaoyueyueReadNum);
+                    log("第" + xiaoyueyuecount + "次," + "已助力完成篇数" + xiaoyueyueReadNum);
 
                     sleep(10000);
 
@@ -7459,9 +7459,7 @@ ui.ok.click(function () {
 
 
                         sleep(3000);
-                        if (联网验证(zwifi) != true) {
-                            连接wifi(zwifi, 5000);
-                        }
+                        连接wifi(zwifi, 5000);
                         打开v();
 
                         let xcx1 = packageName("com.tencent.mm").text("最近使用的小程序").findOnce()
