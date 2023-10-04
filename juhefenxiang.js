a9989a54baf851c99a520a9a1985f599215b6722
q756796724 <756796724@qq.com>
2023-10-05 01:57:45 +0800

    1

--- juhefenxiang.js
+++ juhefenxiang.js
@@ -38,7 +38,12 @@ meitianflag = storage.get("meitianflag", true);
 meitianover = storage.get("meitianover", false);//当天是否完成
 xiaoyueyueflag = storage.get("xiaoyueyueflag", false);
 zfbtx = storage.get("zfbtx", true);
-zfbtxyz = storage.get("zfbtxyz", 5);//阈值
+zfbtxyz = storage.get("zfbtxyz", 3);//阈值
+if(zfbtxyz==5){
+    zfbtxyz=3
+    storage.put("zfbtxyz", zfbtxyz);
+}
+
 xyyzl = storage.get("xyyzl", false);
 xiaoyueyueover = storage.get("xiaoyueyueover", false);//当天是否完成
 xiaoyueyueReadNum = storage.get("xiaoyueyueReadNum", 0);//小阅阅当天阅读次数
@@ -274,7 +279,7 @@ ui.ok.click(function () {
             var MAIN_PKG = "com.fanqie.cloud";
             var PKG_NAME = "com.tencent.mm";
             var MAIN_PAGE = "com.tencent.mm.ui.LauncherUI";
-            var versionNum = "聚合分享v10.5.5";
+            var versionNum = "聚合分享v10.5.6";
             var readNum = 0;//最近获取到的阅读次数
             var retryCount = 0;//进入页面重试次数
             var todayTxCount = 0;
@@ -2463,7 +2468,7 @@ ui.ok.click(function () {
                     nowHour = new Date().getHours();
                     let jb = packageName("com.tencent.mm").className("android.view.View").descMatches(/(.*金币.*)/).findOnce()
                     let jq = packageName("com.tencent.mm").className("android.view.View").descMatches(/(.*收徒奖励.*)/).findOnce()
-                    if (jb && ((zfbtx == true && xyytodayTxCount < 1 && parseFloat(jq.desc().replace(/[^\d.]/g, "")) + parseInt(jb.desc().replace(/[^\d]/g, "")) / 10000 > zfbtxyz) || (zfbtx == false && ((nowHour > 10 && xyytodayTxCount < 1) || (nowHour > 14 && xyytodayTxCount < 2) || (nowHour > 18 && xyytodayTxCount < 3))))) {
+                    if (jb && ((zfbtx == true && xyytodayTxCount < 1 && parseFloat(jq.desc().replace(/[^\d.]/g, "")) + parseInt(jb.desc().replace(/[^\d]/g, "")) / 10000 > zfbtxyz) || (zfbtx == false && ((nowHour > 8 && xyytodayTxCount < 1) || (nowHour > 12 && xyytodayTxCount < 2) || (nowHour > 16 && xyytodayTxCount < 3))))) {
                         click("提现")
                         sleep(10000)
                         if (zfbtx) {
