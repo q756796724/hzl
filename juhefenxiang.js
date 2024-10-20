ce888f9d8573342b2eaef4f5aed4d5049e12bf9c
q756796724 <756796724@qq.com>
2024-10-21 03:24:16 +0800

    1

--- juhefenxiang.js
+++ juhefenxiang.js
@@ -297,7 +297,7 @@ ui.ok.click(function () {
             var MAIN_PKG = "com.fanqie.cloud";
             var PKG_NAME = "com.tencent.mm";
             var MAIN_PAGE = "com.tencent.mm.ui.LauncherUI";
-            var versionNum = "聚合分享v11.4.8";
+            var versionNum = "聚合分享v11.4.9";
             var readNum = 0;//最近获取到的阅读次数
             var retryCount = 0;//进入页面重试次数
             var todayTxCount = 0;
@@ -2727,7 +2727,7 @@ ui.ok.click(function () {
                             连接wifi(zwifi, 5000);
                             return
                         }
-                    } else if (xiaoyueyueurltitle && xiaoyueyueurltitle.text() != null && xiaoyueyueurltitle.text() != "") {
+                    } else if (xiaoyueyueurltitle && xiaoyueyueurltitle.text() != null && xiaoyueyueurltitle.text() != ""&&xiaoyueyueurltitle.text().indexOf("blank_ground")==-1) {
                         xiaoyueyueurltrycount = 0
                         log("小阅阅转载成功")
                         if (zwifi == storage.get("zhuanzaiwifi")) {
